f78a4e87210785c6a1692ad9d3cdf7a92c9741a6
ruilin chen <chenbfr@bc.edu>
2020-08-07 12:36:53 -0400

    set up python environment

---
+++ airbnb_disorder_analytics/config/db_config.py
@@ -0,0 +1,23 @@
+'''
+db_config.py
+with basic setup info for accessing PSQL;
+this is mainly used along with psycopg2 where connections to PSQL can be set up using the following command:
+    >> from db_config import DBInfo
+    >> connection = psycopg2.connect(DBInfo.psycopg2_config)
+    >> cursor = connection.cursor()
+Dependencies:
+    - local package: config
+'''
+
+from . import keys
+import base64
+import importlib
+
+__all__ = ['DBInfo']
+
+class DBInfo:
+    try:
+        importlib.reload(keys)
+        psycopg2_config = "dbname={} user='postgres' host='localhost' password={}".format('up_data', base64.b64decode(keys.psql_password).decode("utf-8"))
+    except UnicodeDecodeError:
+        psycopg2_config = "dbname={} user='postgres' host='localhost' password={}".format('up_data', keys.psql_password)
